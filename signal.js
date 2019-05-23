74f768577d521b644ac3b45a3e96b79c6e719556
Siebe Vanden Eynden <siebe.vandeneynden@nucleus.be>
2019-05-23 21:04:40 +0200

    Added rfid

---
+++ signal.js
@@ -0,0 +1,38 @@
+// Establish a Socket.io connection
+const feathers = require('@feathersjs/feathers');
+const socketio = require('@feathersjs/socketio-client');
+const io = require('socket.io-client');
+
+const socket = io('http://192.168.0.11:3030');
+const client = feathers();
+
+const Gpio = require('onoff').Gpio;
+const button = new Gpio(4, 'in', 'both');
+
+// Initialize our Feathers client application through Socket.io
+// with hooks and authentication.
+
+client.configure(socketio(socket));
+
+// send
+/*
+applicationCache.service('states').patch(2, {
+    data: {
+
+    }
+});
+*/
+
+// listen
+const relayMap = {
+    'browser': new Gpio(5, 'out'),
+    'dns': new Gpio(6, 'out'),
+    'bgp': new Gpio(13, 'out'),
+    'server': new Gpio(19, 'out'),
+};
+client.service('states').on('patched', data => {
+    if(data.hasOwnProperty('panel') && data.hasOwnProperty('valid')){
+        const panel = data.panel;
+        relayMap[panel].writeSync(data.valid);
+    }
+});
